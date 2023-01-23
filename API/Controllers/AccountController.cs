0f4d536ded75a06ed0b57a6be7337801f139fbb4
jimmy adda <addajimmy@gmail.com>
2023-01-23 13:26:34 +0200

    End of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+        public  AccountController (DataContext context, ITokenService tokenService)
+        {
+            this._tokenService = tokenService;
+            _context = context;
+        }
+
+        [HttpPost("register")] //POST: api/Account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.UserName)) return BadRequest("User Name is taken");
+
+            using var hmac = new HMACSHA512(); 
+
+            var user = new AppUser
+            {
+                UserName = registerDto.UserName.ToLower(),
+            
+            };
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+            
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };            
+        }
+
+        [HttpPost("login")] //POST: api/Account/login
+        public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x=>x.UserName == loginDto.UserName);
+
+            if (user == null)  return Unauthorized("Invalid UserName");
+            
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var ComputedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < ComputedHash.Length; i++)
+            {
+                if(ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password!");
+                
+            }
+
+            return new UserDto
+            {
+                UserName = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            }; 
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,32 +1,54 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+
+/****Moved to Extensions-->ApplicationServiceExtensions***/
+
+// builder.Services.AddDbContext<DataContext>(opt => 
+// {
+//  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
+// });
+
+// builder.Services.AddCors();
+// builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+/****Moved to Extensions-->IdentityServiceExtensions***/
+
+/* builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(option =>
 {
- opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+      ValidateIssuer = false,
+      ValidateAudience = false
+    };
 });
+ */
 
-builder.Services.AddCors();
-
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
-}
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+//Authenticate MiddleWare
+app.UseAuthentication(); //Valid token
+app.UseAuthorization(); //token valid , wht are you allowed
+
+
 app.MapControllers();
 
 app.Run();
